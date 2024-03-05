be9c0912e3070775033c13929fcf91741a22e1a6
Bhavyaj2004 <143725856+Bhavyaj2004@users.noreply.github.com>
2024-03-05 14:33:05 +0530

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int binary(int array[],int s,int key){
+    int start =0, end=s-1;
+    while(start <= end){
+        int mid =(start + end)/2;
+        if(array[mid]==key){
+            return mid;
+        } else if(array[mid]<key){
+            start =mid+1;
+        }else{
+            
+        mid =mid-1;
+        }
+    }
+    return 0;
+}
+int main(){
+    int s;
+    cout<<"Enter the size of array:";
+    cin>>s;
+    int array[s];
+    cout<<"Enter the array element:";
+    for(int i=0;i<s;i++){
+        cin>>array[i];
+    }
+    int key;
+    cout<<"Enter the search element:";
+    cin>>key;
+    cout<<"The index of the search element"<<binary(array,s,key);
+    return 0;
+}
+    

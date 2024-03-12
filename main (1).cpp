ec09c924ff1d8a6daebdc1aac1b7ab4f9030dc31
Bhavyaj2004 <143725856+Bhavyaj2004@users.noreply.github.com>
2024-03-12 15:28:23 +0530

    Add files via upload

--- main (1).cpp
+++ main (1).cpp
@@ -1,35 +1,89 @@
-#include<iostream>
+#include <iostream>
+ 
 using namespace std;
-int binary(int array[],int s,int key){
-    int start =0, end=s-1;
-    while(start <= end){
-        int mid =(start + end)/2;
-        if(array[mid]==key){
-            return mid;
-        } else if(array[mid]<key){
-            start =mid+1;
-        }else{
-            
-        mid =mid-1;
+
+void Merge(int *a, int low, int high, int mid
+{
+    
+    int i, j, k, temp[high-low+1]
+    i = low;
+    k = 0;
+    j = mid + 1;
+ 
+    
+    while (i <= mid && j <= high)
+    {
+        if (a[i] < a[j])
+        {
+            temp[k] = a[i];
+            k++;
+            i++;
+        }
+        else
+        {
+            temp[k] = a[j];
+            k++;
+            j++;
         }
     }
-    return 0;
-}
-int main(){
-    cout<<"Name-Bhavya Jain";
-    cout<<"Roll No.-43";
-    int s;
-    cout<<"Enter the size of array:";
-    cin>>s;
-    int array[s];
-    cout<<"Enter the array element:";
-    for(int i=0;i<s;i++){
-        cin>>array[i];
-    }
-    int key;
-    cout<<"Enter the search element:";
-    cin>>key;
-    cout<<"The index of the search element"<<binary(array,s,key);
-    return 0;
-}
+ 
     
+    while (i <= mid)
+    {
+        temp[k] = a[i];
+        k++;
+        i++;
+    }
+ 
+    
+    while (j <= high)
+    {
+        temp[k] = a[j];
+        k++;
+        j++;
+    }
+ 
+    
+    for (i = low; i <= high; i++)
+    {
+        a[i] = temp[i-low];
+    }
+}
+ 
+void MergeSort(int *a, int low, int high)
+{
+    int mid;
+    if (low < high)
+    {
+        mid=(low+high)/2;
+        
+        MergeSort(a, low, mid);
+        MergeSort(a, mid+1, high);
+ 
+        // Merge them to get sorted output.
+        Merge(a, low, high, mid);
+    }
+}
+ 
+int main()
+{
+    int n, i;
+    cout<<"Enter the number of data element to be sorted: ";
+    cin>>n;
+ 
+    int arr[n];
+    for(i = 0; i < n; i++)
+    {
+        cout<<"Enter element "<<i+1<<": ";
+        cin>>arr[i];
+    }
+ 
+    MergeSort(arr, 0, n-1);
+ 
+    
+    cout<<"Sorted Data ";
+    for (i = 0; i < n; i++)
+    cout<<"->"<<arr[i];
+ 
+    return 0;
+}
